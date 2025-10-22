db81c9b4897231f4dfa5291ac540d1d55c61a281
Kirill Tsyganov <kirill.tsyganov@outlook.com.au>
2025-10-22 23:29:07 +1100

    Updates

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/scouts_program_planning_app/sw.js', { scope: '/scouts_program_planning_app/' })})}
